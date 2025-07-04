ecde86715f9b308506f8646e26266420a6c72c5d
Nicolò Pasini <nicolo.pasini90@gmail.com>
2025-07-04 14:16:51 +0200

    chore: project init

---
+++ stories/stories/storiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  storiesApp.swift
+//  stories
+//
+//  Created by nicolo.pasini on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct storiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stories/storiesTests/storiesTests.swift
@@ -0,0 +1,16 @@
+//
+//  storiesTests.swift
+//  storiesTests
+//
+//  Created by nicolo.pasini on 04/07/25.
+//
+
+import Testing
+
+struct storiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
